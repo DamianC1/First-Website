ffed419b47f4cb2b1913b20252d6f28d1142b693
Damian Cygan <damiancygan22@gmail.com>
2023-05-28 17:38:12 +0200

    added menu

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav_item');
+
+const handleNav = () => {
+	nav.classList.toggle('nav--active');
+
+	allNavItems.forEach((item) => {
+		allNavItems.addEventListener('click', () => {
+			nav.classList.remove('nav--active');
+		});
+	});
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle('.nav-items-animation');
+		//animation-delay .3s;
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
+
+navBtn.addEventListener('click', handleNav);
